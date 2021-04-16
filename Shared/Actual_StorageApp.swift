d1fa6030ce67bc6e71bbbc692e911577f4d5ea0c
Bryce Campbell <tonyhawk2100@gmail.com>
2021-04-16 09:39:34 -0700

    Initial Commit

---
+++ Shared/Actual_StorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Actual_StorageApp.swift
+//  Shared
+//
+//  Created by Bryce Campbell on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Actual_StorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
