51b5820e52a17d03166ff129ffb56fac9da12790
Bryce Campbell <tonyhawk2100@gmail.com>
2021-04-16 10:53:31 -0700

    make sure JSON is in right location.

--- Shared/Actual_StorageApp.swift
+++ Shared/Actual_StorageApp.swift
@@ -9,6 +9,19 @@ import SwiftUI
 
 @main
 struct Actual_StorageApp: App {
+    init() {
+        #if os(iOS)
+        let DOCUMENTS_DIRECTORY = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        if !FileManager.default.fileExists(atPath: DOCUMENTS_DIRECTORY.appendingPathComponent("units").appendingPathExtension("json").absoluteString) {
+            
+            let BUNDLE_PATH = Bundle.main.url(forResource: "units", withExtension: "json")
+            
+            try? FileManager.default.copyItem(at: BUNDLE_PATH, to: DOCUMENTS_DIRECTORY.appendingPathComponent("units").appendingPathExtension("json"))
+        }
+        #endif
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
