8894e31d5862cae78c22e5839479568484119c23
Bryce Campbell <tonyhawk2100@gmail.com>
2021-04-17 10:40:52 -0700

    create an error type for Application.

---
+++ Shared/InputError.swift
@@ -0,0 +1,22 @@
+//
+//  InputError.swift
+//  Actual Storage
+//
+//  Created by Bryce Campbell on 4/17/21.
+//
+
+import Foundation
+import SwiftUI
+
+enum InputError: Error {
+    case sizeNotNumerical
+    
+    var alert: Alert? {
+        var errorAlert: Alert? = nil
+        switch self {
+        case .sizeNotNumerical: errorAlert = Alert(title: Text("Input Not Numerical"), message: Text("Input must be numerical"), dismissButton: .default(Text("Ok")))
+        }
+        
+        return errorAlert
+    }
+}
