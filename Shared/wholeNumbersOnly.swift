83edda7834616da450357baba8bde2e4eef3241c
Bryce Campbell <tonyhawk2100@gmail.com>
2021-04-17 10:21:47 -0700

    work on class to get proper input.

---
+++ Shared/wholeNumbersOnly.swift
@@ -0,0 +1,13 @@
+//
+//  wholeNumbersOnly.swift
+//  Actual Storage
+//
+//  Created by Bryce Campbell on 4/17/21.
+//
+
+import Foundation
+import Combine
+
+class WholeNumbersOnly: ObservableObject {
+    
+}
